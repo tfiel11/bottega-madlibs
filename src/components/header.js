9e1d4db33bebc709a449e939da5aa8e0357a489e
Tyler Fielding <ty.fielding@gmail.com>
2018-06-12 16:58:01 -0600

    built our first functional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib story</p>
+        </div>    
+    )
+}
+
+export default Header; 
