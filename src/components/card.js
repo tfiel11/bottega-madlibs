acf4d2f830470759dda7f811caa2ff4f75bb7a6c
Tyler Fielding <ty.fielding@gmail.com>
2018-06-14 10:28:09 -0600

    created inputs for card

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -62,6 +63,7 @@ class Card extends Component {
                 {
                     inputData.map(data => Input( (data), this.handleInputChange ))
                 }
+                <Content data={this.state}/>
             </div>    
         )
     }
---
+++ src/components/content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        const data = this.props.data;
+        return(
+            <p>
+
+            </p>
+        )
+    )
+}
+
+export default Content;
