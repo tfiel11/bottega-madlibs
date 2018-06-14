0d6bc1e8508ec531713496eb33dda957dcc070fa
Tyler Fielding <ty.fielding@gmail.com>
2018-06-14 16:07:15 -0600

    created a css grid for card and inputs

--- src/components/input.js
+++ src/components/input.js
@@ -5,7 +5,7 @@ const Input = ({title, state, name}, onChange, index) => {
         <div key={index} className="input">
             <label className="input__number">{index + 1}</label>
             <input name={name} value={state} onChange={onChange}/>
-            <label> className="input__title">{title}</label>
+            <label className="input__title">{title}</label>
         </div>    
     )
 }
